e1384d1fa9658a90476d1c158d558a859ea71b9c
smatalha <sma.talha@gmail.com>
2020-06-10 18:08:24 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,53 @@
 class StudentsController < ApplicationController
+  before_action :find_student, only: [:show, :edit, :update, :destroy]
 
   def index
-    @students = Student.all
+      @students = Student.all 
+  end
+
+  def show
+  end
+
+  def new
+      @student = Student.new
+  end
+
+  def create
+      student = Student.create(student_params)
+
+      if student.valid?
+        redirect_to student_path(student)
+      else
+        flash[:student_errors] = student.errors.full_messages
+        redirect_to new_student_path
+      end
+
+      # redirect_to student_path(student)
+  end
+
+  def edit
+  end
+
+  def update
+    @student.update(student_params)
+
+    redirect_to student_path(@student)
+  end
+
+  def destroy
+    @student.destroy
+
+    redirect_to students_path
+  end
+
+  private
+
+  def student_params
+      params.require(:student).permit(:name, :mod)
+  end
+
+  def find_student
+    @student = Student.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :ducks, only: [:index]
-  resources :students, only: [:index]
+  resources :ducks#, only: [:index]
+  resources :students#, only: [:index, :show, :new, :create, :edit, :update]
+
+  # get '/ducks', to: 'ducks#index', as: "ducks"
+  # get '/ducks/new', to: 'ducks#new', as: "new_duck"
+  # get '/ducks/:id', to: 'ducks#show', as: "duck"
+  # post '/ducks', to: 'ducks#create'
+  # get '/ducks/:id/edit', to: 'ducks#edit'
 end
