4b214b0d8b9d3e02666108f67fe77d97961b260b
JeanmarieJackman <jeanmarie.jackman@gmail.com>
2020-06-09 20:45:45 -0400

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
+Student.delete_all
+Duck.delete_all 
+
+student_ids = []
 10.times do
-  Student.create(name: Faker::Name.name, mod: rand(1..5))
+  s = Student.create(name: Faker::Name.name, mod: rand(1..5))
+  student_ids << s.id
 end
 
+
 5.times do
-  Duck.create(name: Faker::Creature::Dog.meme_phrase, description: Faker::Creature::Dog.breed)
+  Duck.create(
+    name: Faker::Creature::Dog.meme_phrase, 
+    description: Faker::Creature::Dog.breed, 
+    student_id: (student_ids).sample)
 end
