c6f749969a644f0e0186154ff31d5fd735668bb7
aromsa <annacbateman@gmail.com>
2020-06-11 11:08:30 -0400

    Done.

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -4,4 +4,45 @@ class DucksController < ApplicationController
     @ducks = Duck.all
   end
 
+  def show
+    @duck = Duck.find(params[:id])
+  end
+
+  def new
+    @duck = Duck.new
+    @students = Student.all
+  end
+
+  def create
+    duck = Duck.create(duck_params)
+    if duck.valid?
+      redirect_to student_path(duck.student_id)
+    else
+      flash[:duck_errors] = duck.errors.full_messages
+      redirect_to new_duck_path
+    end
+  end
+
+  def edit
+    @duck = Duck.find(params[:id])
+    @students = Student.all
+  end
+
+  def update
+    duck = Duck.find(params[:id])
+    duck.update(duck_params)
+    if duck.valid?
+      redirect_to duck_path(duck)
+    else
+      flash[:duck_errors] = duck.errors.full_messages
+      redirect_to edit_duck_path
+    end
+  end
+
+  private
+
+  def duck_params
+    params.require(:duck).permit!
+  end
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,11 @@
 class Student < ApplicationRecord
+  has_many :ducks
+
+  validates :name, presence: true
+  validates :mod, numericality: {less_than_or_equal_to: 5}
+  validates :mod, numericality: {greater_than: 0}
+
+  def ducks
+    Duck.all.select{|d| d.student == self}
+  end
 end
